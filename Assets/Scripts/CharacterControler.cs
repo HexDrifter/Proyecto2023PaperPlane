b46bd9a8f401639820e91e8d6e3e186e6fcd0d5c
HexDrifter <42625632+HexDrifter@users.noreply.github.com>
2023-07-07 14:33:33 -0400

    Actualizados cambios comportamiento nave
    Ahora la nave se mueve en un area cerrada, aún así no puede disparar ya que la bala está en proceso de creación

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -1,24 +1,54 @@
+using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterControler : MonoBehaviour
 {
-    public float velocidad;
+    [SerializeField] public float velocidad; 
+    [SerializeField] public CinemachineBrain brain;
+    public Rigidbody2D rbody;
+    public Vector2 inputMove; //Hice la variable pública para ver el input
 
+
+
+    public void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        //brain = GetComponent<CinemachineBrain>();
+    }
     void Update()
     {
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shotBullet();
+        }
+    }
+
+    private void FixedUpdate()
+    {
         ProcesarMovimiento();
+        brain.ManualUpdate();
+        
+    }
+
+    private void shotBullet()
+    {
+        Debug.Log("Bang");
+        //throw new NotImplementedException();
     }
 
     // Update is called once per frame
     void ProcesarMovimiento()
     {
-        float inputHorizontal = Input.GetAxis("Horizontal");
-        float inputVertical = Input.GetAxis("Vertical");
+        inputMove.x = Input.GetAxis("Horizontal");
+        inputMove.y = Input.GetAxis("Vertical");
 
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = inputMove * velocidad;
 
-        rigidbody.velocity = new Vector2(inputHorizontal * velocidad, inputVertical * velocidad);
+        //rbody.velocity = new Vector2(inputHorizontal * velocidad, inputVertical * velocidad);
     }
 }
