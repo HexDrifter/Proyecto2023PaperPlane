c1a413776be33f8de9c2a5167e7fb6465edc8202
JMezaV2020 <josemeza@alumnos.upla.cl>
2023-07-14 03:24:33 -0400

    D

--- Assets/Scripts/NubeObstaculo.cs
+++ Assets/Scripts/NubeObstaculo.cs
@@ -12,51 +12,25 @@ public class NubeObstaculo : MonoBehaviour
 
     private void Start()
     {
-        // Determinar la dirección de movimiento aleatoriamente
         if (moverHorizontal)
         {
-            direccion = (Random.value < 0.5f) ? Vector3.right : Vector3.left;
+            direccion = esPositivo ? Vector3.right : Vector3.left;
         }
         else if (moverVertical)
         {
-            direccion = (Random.value < 0.5f) ? Vector3.up : Vector3.down;
+            direccion = esPositivo ? Vector3.up : Vector3.down;
         }
     }
 
     private void Update()
     {
         transform.position += direccion * speed * Time.deltaTime;
-
-        // Verificar si alcanzó el límite y cambiar la dirección
-        if (moverHorizontal)
-        {
-            if (esPositivo && transform.position.x >= 10f)
-            {
-                direccion = Vector3.left;
-            }
-            else if (!esPositivo && transform.position.x <= -10f)
-            {
-                direccion = Vector3.right;
-            }
-        }
-        else if (moverVertical)
-        {
-            if (esPositivo && transform.position.y >= 10f)
-            {
-                direccion = Vector3.down;
-            }
-            else if (!esPositivo && transform.position.y <= -10f)
-            {
-                direccion = Vector3.up;
-            }
-        }
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "square")
         {
-            // Cambiar la dirección del objeto
             direccion = -direccion;
         }
     }
