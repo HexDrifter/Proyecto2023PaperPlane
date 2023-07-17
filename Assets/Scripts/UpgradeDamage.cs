32b399baab59ca305b8035b3013beed7a0e54d60
JMezaV2020 <josemeza@alumnos.upla.cl>
2023-07-17 16:59:20 -0400

    Cosas

--- Assets/Scripts/UpgradeDamage.cs
+++ Assets/Scripts/UpgradeDamage.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class UpgradeShoot : MonoBehaviour
 {
-    void upgradeShot(levelUpgradeShot)
+    /*void upgradeShot(levelUpgradeShot)
     {
         if (levelUpgradeShot >= 0 && levelUpgradeShot <= 4)
         {
             ShotDamage = damage+5;
-            levelUpgradeShot+1
+            levelUpgradeShot+1;
         }
     }
     void upgradeShot(levelUpgradeBomb)
@@ -17,7 +17,7 @@ public class UpgradeShoot : MonoBehaviour
         if (levelUpgradeBomb >= 0 && levelUpgradeBomb <= 4)
         {
             BombDamage = damage + 5;
-            levelUpgradeBomb+1
+            levelUpgradeBomb+1;
         }
-    }
+    }*/
 }
