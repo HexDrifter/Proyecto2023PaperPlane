1e370a73e89270ceb182bad7bd197ca71de29c45
Esteban Adolfo Ponce Lira <esteban.ponce@alumnos.upla.cl>
2023-07-17 14:42:35 -0400

    Script UpgradeDamage v1

---
+++ Assets/Scripts/UpgradeDamage.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeShoot : MonoBehaviour
+{
+    void upgradeShot(levelUpgradeShot)
+    {
+        if (levelUpgradeShot >= 0 && levelUpgradeShot <= 4)
+        {
+            ShotDamage = damage+5;
+            levelUpgradeShot+1
+        }
+    }
+    void upgradeShot(levelUpgradeBomb)
+    {
+        if (levelUpgradeBomb >= 0 && levelUpgradeBomb <= 4)
+        {
+            BombDamage = damage + 5;
+            levelUpgradeBomb+1
+        }
+    }
+}
