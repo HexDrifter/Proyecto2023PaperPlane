6313db0db16b97bdc501c537e4feb77396b64f47
Esteban Adolfo Ponce Lira <esteban.ponce@alumnos.upla.cl>
2023-07-17 17:09:24 -0400

    17-07

--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class DestroyEnemy : MonoBehaviour
 {
-    void destroyEnemy(enemyID, HPEnemy, enemyMoney)
+    /*void destroyEnemy(enemyID, HPEnemy, enemyMoney)
     {
-        // 
-        HPEnemy = HPEnemy - damageShot;
-        HPEnemy = HPEnemy - damageBomb;
         if (HPEnemy < 1)
         {
             //Destruir al enemigo en especifico con su ID
             
 
         }
-    }
+    }*/
 }
