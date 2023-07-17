d823a549f7fbb45bfaed45f464d849bfb11cdf8d
Esteban Adolfo Ponce Lira <esteban.ponce@alumnos.upla.cl>
2023-07-17 17:25:58 -0400

    17-07

--- Assets/Scripts/BombBehavior.cs
+++ Assets/Scripts/BombBehavior.cs
@@ -6,13 +6,33 @@ using UnityEngine;
 public class bombBehavior : Enemy
 {
     public int bombDamage = 100;
+
+
     void useBomb()
     {
-        
+        /*// Obtén todos los objetos en la escena con el tag "Enemy"
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        // Itera sobre todos los enemigos y reduce su vida
+        foreach (GameObject enemy in enemies)
+        {
+            // Obtén una referencia al script de vida del enemigo
+            EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
+
+            // Reducir la vida del enemigo llamando a la función correspondiente en su script
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(bombDamage);
+            }
+        }*/
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+
     }
     private void Update()
     {
-        if (Input.GetKey("ctrl"))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             useBomb();
         }
