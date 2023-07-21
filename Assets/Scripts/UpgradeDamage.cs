618fd7b07d1ff2bf372bcdbfb23b3ec1694d7ce5
Multinecz <nicolas.cepeda@alumnos.upla.cl>
2023-07-20 21:03:31 -0400

    Update UpgradeDamage.cs

--- Assets/Scripts/UpgradeDamage.cs
+++ Assets/Scripts/UpgradeDamage.cs
@@ -9,15 +9,20 @@ public class UpgradeShoot : MonoBehaviour
         if (levelUpgradeShot >= 0 && levelUpgradeShot <= 4)
         {
             ShotDamage = damage+5;
-            levelUpgradeShot+1;
+            levelUpgradeShot + 1;
         }
     }
     void upgradeShot(levelUpgradeBomb)
     {
         if (levelUpgradeBomb >= 0 && levelUpgradeBomb <= 4)
         {
+<<<<<<< HEAD
             BombDamage = damage+5;
             levelUpgradeBomb+1;
+=======
+            BombDamage = damage + 5;
+            levelUpgradeBomb + 1;
+>>>>>>> 189a9ad50b4dfafd2f4e7a3576d33873a04ff263
         }
     }
 }
