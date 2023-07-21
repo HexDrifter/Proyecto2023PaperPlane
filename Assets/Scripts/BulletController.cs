a95cad1bf79b646f0ab5a51caaa53a77b6b79e4e
HexDrifter <42625632+HexDrifter@users.noreply.github.com>
2023-07-20 22:29:46 -0400

    Aircraft updated

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public Vector2 bulletDirection;
+    [SerializeField] public Vector2 bulletDirection;
     public float bulletSpeed;
     public float bulletDamage;
+    private Rigidbody2D rb;
 
-    
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 5 );
+    }
+
+    public BulletController(Vector2 Direction)
+    {
+        bulletDirection = Direction;
+    }
 
     // Update is called once per frame
     public void Update()
     {
-        
+        rb.velocity = transform.right * bulletDirection * 100;
     }
 }
--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -11,6 +11,7 @@ public class CharacterControler : MonoBehaviour
     public Rigidbody2D rbody;
     public Vector2 inputMove; // Hice la variable pública para ver el input
     public GameObject bullet;
+    [SerializeField] public float bulletSpeed;
 
     public void Start()
     {
@@ -38,7 +39,16 @@ public class CharacterControler : MonoBehaviour
         Vector2 bulletPos = transform.position;
         bulletPos.x += 0.1f;
         Instantiate(bullet, bulletPos, Quaternion.identity);
-        //throw new NotImplementedException();
+        //Vector2 bulletDirection;
+        //bulletDirection = new Vector2(1,0)
+        
+
+
+        Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+
+        // Aplicar velocidad a la bala en la dirección hacia arriba (puedes ajustar esto según tus necesidades)
+        rbBullet.velocity = transform.right * bulletSpeed * Time.deltaTime;
+        
     }
 
     void ProcesarMovimiento()
