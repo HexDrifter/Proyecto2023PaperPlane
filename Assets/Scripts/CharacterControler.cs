6f0824454b928d68246dc595bcd4952ebc2c92a8
Esteban Adolfo Ponce Lira <66049549+HatiBerg@users.noreply.github.com>
2023-07-21 00:58:02 -0400

    Add BombBehavior.cs to script

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -13,6 +13,12 @@ public class CharacterControler : MonoBehaviour
     [SerializeField] public GameObject bulletObject;
     [SerializeField] public int bulletDamage;
     [SerializeField] public float bulletSpeed;
+    [SerializeField] public int bombDamage;
+    [SerializeField] public int currentBombs;
+    [SerializeField] public int maxBombs;
+    private bool isCooldown = false;
+    private float lastUsedTime = 0f;
+    [SerializeField] private float bombCooldownDuration;
 
     public void Start()
     {
@@ -25,6 +31,16 @@ public class CharacterControler : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             shotBullet();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isCooldown && currentBombs > 0)
+        {
+            UseBomb();
+        }
+
+        // Verifica si el cooldown ha terminado
+        if (isCooldown && Time.time - lastUsedTime >= bombCooldownDuration)
+        {
+            isCooldown = false;
         }
     }
 
@@ -55,5 +71,32 @@ public class CharacterControler : MonoBehaviour
         inputMove.y = Input.GetAxis("Vertical");
 
         rbody.velocity = inputMove * velocidad;
+    }
+
+    void UseBomb()
+    {
+        // Obtiene todos los colliders dentro de la zona con el tag "MainCamera" y los almacena en la variable "targets"
+        Collider2D[] targets = Physics2D.OverlapBoxAll(transform.position, transform.localScale / 2, 0f, LayerMask.GetMask("MainCamera"));
+
+        foreach (Collider2D target in targets)
+        {
+            if (target.CompareTag("Enemy"))
+            {
+                Destroy(target.gameObject);
+                /*Enemy enemy = target.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    enemy.getDamage(bombDamage);
+                }*/
+            }
+            if (target.CompareTag("EnemyBullet"))
+            {
+                Destroy(target.gameObject);
+            }
+        }
+        currentBombs--;
+        //Cooldown para no spamear la bomba
+        isCooldown = true;
+        lastUsedTime = Time.time;
     }
 }
