b3e3f13d3fad45172a5166a7094120bb28148d66
Multinecz <nicolas.cepeda@alumnos.upla.cl>
2023-08-11 10:39:46 -0400

    Update Balon Basket

--- Assets/Scripts/balonbasket.cs
+++ Assets/Scripts/balonbasket.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class balonbasket : Enemy, IDamageable
+public class balonbasket : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public Vector2 initialLaunch;
+    public float speed;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(initialLaunch * speed);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, (float)(gameObject.transform.position.y + 0.05), gameObject.transform.position.z);
+        
     }
 }
